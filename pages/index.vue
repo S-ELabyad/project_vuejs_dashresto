2618cf170f8c19c08c84eb3f078fd06110b4343d
S-ELabyad <S-ELabyad>
2022-01-12 13:53:48 +0100

    second commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <v-app id="inspire">
+      <drawer />
+      <bank />
+      <dash21 />
+    </v-app>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Index',
+  data () {
+  }
+
+}
+</script>
